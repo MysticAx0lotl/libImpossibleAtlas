a1ae44b62c11ae9a4f090445baf2d325fc869612
MysticAx0lotl <techguy791@gmail.com>
2025-04-18 11:22:07 -0600

    move unused code to its own file

--- src/libImpossibleAtlas.cpp
+++ src/libImpossibleAtlas.cpp
@@ -268,23 +268,6 @@ void ImageAtlas::loadBin(std::vector<unsigned char> atlasChars, bool debugMode)
             }
 
             std::cout << "Loaded " << tempImg->FragmentArr.size() << " fragment(s)!" << std::endl;
-
-            //REMOVE BEFORE RELEASE
-            /*
-            Image *cstbg = new Image;
-
-            Fragment *cust = new Fragment;
-            cust->x_short_1 = 0.892578125;
-            cust->y_short_2 = 0.45703125;
-            cust->w_short_3 = 0.078125;
-            cust->h_short_4 = 0.533203125;
-        
-            cust->name_utf_0 = "CstBgtest";
-
-            tempImg->FragmentArr.push_back(*cust);
-            tempImg->fragmentArrLen++;
-            this->numFragments++;
-            */
             
             this->ImagesArr.push_back(*tempImg);           
         }
